433af83137772f5265ce147599d301b51de83f57
AnasBelaaraj <anasbel2000@hotmail.fr>
2021-11-20 09:51:40 +0100

    Ramon's update

--- Clubmanagement/src/main/java/com/example/Clubmanagement/entities/club/evenement.java
+++ Clubmanagement/src/main/java/com/example/Clubmanagement/entities/club/evenement.java
@@ -1,5 +1,57 @@
 package com.example.Clubmanagement.entities.club;
 
 
+import org.hibernate.annotations.NotFound;
+
+import javax.persistence.*;
+import java.awt.*;
+import java.lang.management.GarbageCollectorMXBean;
+
+@Entity
+@Table(name = "Events")
 public class evenement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id_event;
+    private String Description;
+
+
+
+    public long getId_event() {
+        return id_event;
+    }
+
+    public void setId_event(long id_event) {
+        this.id_event = id_event;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+
+
+
+    public evenement(long id_event, String description) {
+
+        this.id_event = id_event;
+        Description = description;
+
+    }
+
+    public evenement() {
+    }
+
+    public evenement(String description) {
+        Description = description;
+
+    }
+
+
+    @OneToOne(mappedBy = "Ev")
+    private  Club c;
 }
