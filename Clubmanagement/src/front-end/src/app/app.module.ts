76a7ddd3bf36be3f96fe74894052b4f4af0a521c
AnasBelaaraj <anasbel2000@hotmail.fr>
2021-12-03 00:54:13 +0100

    fixing appmodule

--- Clubmanagement/src/front-end/src/app/app.module.ts
+++ Clubmanagement/src/front-end/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
