deb1f7e889492546c1ea917aa304995be488f098
AnasBelaaraj <anasbel2000@hotmail.fr>
2021-12-08 00:30:31 +0100

    adding night mode function to landing page

--- Clubmanagement/src/front-end/src/app/examples/landing/landing.component.ts
+++ Clubmanagement/src/front-end/src/app/examples/landing/landing.component.ts
@@ -14,4 +14,26 @@ export class LandingComponent implements OnInit {
 
   ngOnInit() {}
 
+    timeVerifyNight(){
+      let date: Date = new Date();
+      var currentHour=date.getHours();
+      if (currentHour>=19 && currentHour<6){
+        return true;
+      }
+      else {
+          return false;
+      }
+    }
+
+    timeVerifyDay(){
+      let date: Date = new Date();
+      var currentHour=date.getHours();
+      if (currentHour>=6 && currentHour<19){
+        return true;
+      }
+      else {
+          return false;
+      }
+    }
+
 }
