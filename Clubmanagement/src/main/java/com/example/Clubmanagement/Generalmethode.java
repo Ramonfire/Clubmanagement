48f41318633c168e972b39da8fee6c54fec32f9d
Ramonfire <omar.zaida@uir.ac.ma>
2021-12-16 21:00:17 +0100

    refined Visitor controls . except signup !  ready to deploy to the front end

--- Clubmanagement/src/main/java/com/example/Clubmanagement/Generalmethode.java
+++ Clubmanagement/src/main/java/com/example/Clubmanagement/Generalmethode.java
@@ -1,62 +1,13 @@
 package com.example.Clubmanagement;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.example.Clubmanagement.entities.club.Club;
-import com.example.Clubmanagement.entities.compte.generlAc.Compte;
-import com.example.Clubmanagement.entities.compte.generlAc.Role;
-import com.example.Clubmanagement.services.AccountService;
-import com.example.Clubmanagement.services.ClubService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequiredArgsConstructor
 @Slf4j
 public class Generalmethode {
 
-    private final AccountService accountService_togetaccount;
 
-    public   Compte getaccoutThroughheader(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String authorisationHeader = request.getHeader(AUTHORIZATION);
-        if (authorisationHeader != null && authorisationHeader.startsWith("Bearer ")) {
-            try {
-                String refresh_Token = authorisationHeader.substring("Bearer ".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-                JWTVerifier verifier = JWT.require(algorithm).build();
-                DecodedJWT decodedJWT = verifier.verify(refresh_Token);
-                String email = decodedJWT.getSubject();
-                Compte user = this.accountService_togetaccount.getAccountbymail(email);
-
-                return user;
-            } catch (Exception exception) {
-                response.setHeader("error in {}", exception.getMessage());
-                response.setStatus(NOT_FOUND.value());
-                //response.sendError(FORBIDDEN.value());
-                Map<String, String> error = new HashMap<>();
-                error.put("error_message", exception.getMessage());
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), error);
-            }
-
-
-        } else throw new RuntimeException("Refresh Token ismissing");
-
-        return new Compte();
-    }
 }
