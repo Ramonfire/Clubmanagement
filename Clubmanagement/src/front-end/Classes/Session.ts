523c9c949f657a487e450a18f774ba74557799c7
Ramonfire <omar.zaida@uir.ac.ma>
2021-12-28 02:31:35 +0100

    created session problem with authentification form to be reviewed

--- Clubmanagement/src/front-end/Classes/Session.ts
+++ Clubmanagement/src/front-end/Classes/Session.ts
@@ -3,6 +3,19 @@ class Session {
    private _refresh_token:String;
 
 
+   get access_token(): String {
+      return this._access_token;
+   }
 
+   set access_token(value: String) {
+      this._access_token = value;
+   }
 
+   get refresh_token(): String {
+      return this._refresh_token;
+   }
+
+   set refresh_token(value: String) {
+      this._refresh_token = value;
+   }
 }
