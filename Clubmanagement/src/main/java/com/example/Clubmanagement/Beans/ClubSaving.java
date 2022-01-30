8c19017544f54c52aa23c27274c2d2788dc72296
Ramonfire <omar.zaida@uir.ac.ma>
2022-01-30 04:04:30 +0100

    added budget. code cleaning

--- Clubmanagement/src/main/java/com/example/Clubmanagement/Beans/ClubSaving.java
+++ Clubmanagement/src/main/java/com/example/Clubmanagement/Beans/ClubSaving.java
@@ -2,8 +2,10 @@ package com.example.Clubmanagement.Beans;
 
 import com.example.Clubmanagement.Repositories.ClubRepo;
 import com.example.Clubmanagement.Repositories.EtudiantRepo;
+import com.example.Clubmanagement.Repositories.RpedaRepo;
 import com.example.Clubmanagement.entities.club.Club;
 import com.example.Clubmanagement.entities.compte.generlAc.Etudiant;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +13,9 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class ClubSaving {
+    private final RpedaRepo repo1;
 
     @Bean
     CommandLineRunner commandLineRunnerClub(ClubRepo repo) {
@@ -19,6 +23,9 @@ public class ClubSaving {
         Club C1 = new Club("Chillin side",Boolean.TRUE," Games.and.Movies ");
         Club C2 = new Club("Rotarac",Boolean.TRUE," Donor ");
         Club C3 = new Club("Bridge",Boolean.FALSE," Media ");
+        C1.setPed(repo1.getById(Long.valueOf(1)));
+        C2.setPed(repo1.getById(Long.valueOf(2)));
+        C3.setPed(repo1.getById(Long.valueOf(1)));
 
 repo.saveAll(List.of(C1,C2,C3));
         return args -> {
